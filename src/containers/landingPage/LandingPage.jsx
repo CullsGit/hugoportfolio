a5b6ff05b8ce2d40cd3b2a81ada8bfa52fd21540
Hugo Cullinan <hugo.cullinan@gmail.com>
2024-03-01 09:47:47 +1100

    add border for landing page

--- src/containers/landingPage/LandingPage.jsx
+++ src/containers/landingPage/LandingPage.jsx
@@ -4,9 +4,17 @@ import "./landingPage.css";
 
 export default function LandingPage() {
   return (
-    <div className="landing-page d-flex flex-column min-vh-100 justify-content-center align-items-center">
-      <h1>Hello, my name is Hugo Cullinan. I'm a keen Web Developer.</h1>
-      <button>About me</button>
-    </div>
+    <>
+      <div className="landing-page d-flex flex-column min-vh-100 justify-content-center align-items-center">
+        <div className="landing-page__border" />
+        <h1>
+          Hello, my name is <span className="hugo-text">Hugo Cullinan</span> I'm
+          a keen Web Developer.
+        </h1>
+        <button type="button" className="btn btn-primary mt-5">
+          About me
+        </button>
+      </div>
+    </>
   );
 }
