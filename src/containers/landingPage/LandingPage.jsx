619dc227aada1f7d2470021be3a14a3fd10d7790
Hugo Cullinan <hugo.cullinan@gmail.com>
2024-03-20 16:23:17 +1100

    change border to wrap content

--- src/containers/landingPage/LandingPage.jsx
+++ src/containers/landingPage/LandingPage.jsx
@@ -6,21 +6,22 @@ import "./landingPage.css";
 export default function LandingPage() {
   return (
     <>
-      <div className="container landing-page d-flex flex-column min-vh-100 justify-content-center align-items-center">
-        <div className="landing-page__border" />
-        <div className="landing-page__content">
-          <div className="landing-page__text">
-            <h1>Hugo Cullinan</h1>
-            <p>Hello! I am a keen Web Developer.</p>
+      <div className="container min-vh-100 landing-page d-flex flex-column justify-content-center align-items-center">
+        <div className="landing-page__border">
+          <div className="landing-page__content">
+            <div className="landing-page__text">
+              <h1>Hugo Cullinan</h1>
+              <p>Hello! I am a keen Web Developer.</p>
+            </div>
+            <div>
+              <img src={headshot} />
+            </div>
           </div>
-          <div>
-            <img src={headshot} />
-          </div>
-        </div>
 
-        <button type="button" className="btn btn-primary mt-5">
-          Projects
-        </button>
+          <button type="button" className="btn btn-primary btn-lg mt-5">
+            Projects
+          </button>
+        </div>
       </div>
     </>
   );
