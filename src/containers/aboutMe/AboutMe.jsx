63db2d4448a5c744da27264d7a9b2497576a8146
Hugo Cullinan <hugo.cullinan@gmail.com>
2024-03-22 11:46:31 +1100

    add layout styling for about page

--- src/containers/aboutMe/AboutMe.jsx
+++ src/containers/aboutMe/AboutMe.jsx
@@ -5,27 +5,23 @@ import "./aboutMe.css";
 
 export default function AboutMe() {
   return (
-    <div className="container min-vh-100" id="about">
+    <div className="container min-vh-100 d-flex align-items-center" id="about">
       <div className="about-page__border">
-        <div className="about-page__content">
-          <div className="about-page__text">
-            <h1>About Me</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint,
-              delectus. A ducimus dicta eos voluptatibus, doloribus modi
-              suscipit deserunt dolorum et mollitia libero ipsum nisi facere
-              laboriosam dolorem omnis rem. Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Sint, delectus. A ducimus dicta eos
-              voluptatibus, doloribus modi suscipit deserunt dolorum et mollitia
-              libero ipsum nisi facere laboriosam dolorem omnis rem.Lorem ipsum
-              dolor sit amet consectetur adipisicing elit. Sint, delectus. A
-              ducimus dicta eos voluptatibus, doloribus modi suscipit deserunt
-              dolorum et mollitia libero ipsum nisi facere laboriosam dolorem
-              omnis rem.
-            </p>
-          </div>
-          <div className="about-page__img">
+        <div className="about-page__content row">
+          <div className="about-page__img col">
             <img src={desk} alt="desk" className="img-fluid" />
+          </div>
+          <div className="about-page__text col">
+            <h1 className="display-3">About Me</h1>
+            <p>
+              Hi, I'm Hugo, a passionate web developer specializing in React
+              applications and front-end development. With a keen eye for detail
+              and a love for clean, efficient code, I strive to create engaging
+              user experiences that delight and inspire. Whether it's crafting
+              responsive layouts, implementing complex animations, or optimizing
+              performance, I'm dedicated to delivering high-quality solutions
+              that exceed expectations. Let's build something amazing together!
+            </p>
           </div>
         </div>
       </div>
